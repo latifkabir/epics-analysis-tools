94ac95460f3253bb48ea28bd9f22144a06d8fd30
Latif Kabir <kabir@bnl.gov>
2024-06-12 14:49:32 -0400

    More testing

--- src/GrootPvaDemo.java
+++ src/GrootPvaDemo.java
@@ -1,8 +1,10 @@
 import org.phoebus.pv.PV;
 import org.phoebus.pv.PVPool;
 import org.phoebus.core.vtypes.VTypeHelper;
+import org.epics.vtype.Time;
 
 import java.util.concurrent.CountDownLatch;
+import java.time.Instant;
 
 import org.jlab.groot.data.*;
 import org.jlab.groot.ui.*;
@@ -24,11 +26,14 @@ public class GrootPvaDemo
             CountDownLatch connect = new CountDownLatch(1000);
             pv.onValueEvent().subscribe(value ->
             {
-                System.out.println(pv.getName() + " = " + value);
+                // System.out.println(pv.getName() + " = " + value);
 		final double val = VTypeHelper.toDouble(value);
-                System.out.println(val);
+		final Instant time = VTypeHelper.getTimestamp(value);
+		double msec = time.toEpochMilli();
+		
+                // System.out.println(val + " at " + time);
 
-		gr.addPoint(val + 1, val, 0, 0);
+		gr.addPoint(msec, val, 0, 0);
 		//++counter;
 		c1.draw(gr);
 
